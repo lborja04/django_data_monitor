1318f4dd43321f3abf906cfbab8964fb564a3698
Luis Borja <139704941+lborja04@users.noreply.github.com>
2025-08-22 22:32:31 +0000

    cambios

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -123,13 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
+# Carpeta donde tienes tus archivos de desarrollo (ej. project/static/)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATIC_URL),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = BASE_DIR / 'assets'
+# Carpeta de destino para collectstatic en producción
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
